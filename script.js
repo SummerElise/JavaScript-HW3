7896a9b4c58ce4a28d523168f8447eeae3ec71e5
lissy <slissy513@gmail.com>
2021-04-17 14:56:04 -0400

    added comments to my script

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-
+//Arrays
 var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var symbolArray = ["^", "%", "$", "#", "@", "&", "*", "(", ")", "+", "=", "_", "-"];
 var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var UpperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var Outcome = [];
 
+//Added else so the prompt would restart when invalid input is alerted
 var Passwordlength = prompt("Choose a minimum of 8 characters and a maximum of 128 characters.");
 if (Passwordlength < 8 || Passwordlength > 128) {
+  //Alert prompted on invalid choice input
   alert("Invalid choice. Please try again.");
   ask();
 }
@@ -32,6 +34,7 @@ var numbers =confirm ("Would you like numbers in your password?");
     Outcome = Outcome.concat(numericArray);
 };
 
+//Creates a random Password
 var RandomNewPassword = [];
 for (let i=0; i < Passwordlength; ++i) {
   RandomNewPassword.push (Outcome[Math.floor(Math.random() * Outcome.length)]);
