5b60cbb7fcc3ada1150a2fcaa2f51f087db2545f
lissy <slissy513@gmail.com>
2021-04-16 23:13:09 -0400

    added event listeners and making new alerts for wrong input

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var length 
-var Strings = [];
+
+// Different Arrays
 var numberString = "0123456789";
 var UpperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerString = "abcdefghijklmnopqrstuvwxyz";
 var symbolsString = "!@#$%^&*(){}[]=<>/,.";
+var randomPswdString ='';
+
+ 
+var number;
+var Upper;
+var lower;
+var symbols;
 
 // Write password to the #password input
 function generatePassword() {
   var generateBtn = prompt("Choose a minimum of 8 characters and a maximum of 128 characters.");
 
-  if (generateBtn < 8 || generateBtn > 128) {
-    alert("Invalid choice. Please try again.") 
-  }
-   else {
-
-  
-  var numberString = confirm("Would you like your password to include numbers?");
-  if(numberString === true) {
-    for(var i = 0; i < numberString.length; i++)
-    Strings.push(numberString[i]);
-    }
-  
-  var UpperString = confirm("Would you like your password to include Uppercase letters?");
-  if (UpperString === true) {
-    for (var i = 0; i < UpperString.length; i++);
-  }
-  var lowerString = confirm("Would you like your password to include lowercase letters?");
-  if (lowerString === true) {
-    for (var i = 0; i < lowerString.length; i++);
-  }
-  var symbolsString = confirm("Would you like your password to include special characters?");
-  if (symbolsString === true) {
-    for (var i = 0; i < symbolsString.length; i++);
+  if(length < 8 || generateBtn > 128 || typeof(length) !=number || length === NaN || length === null) {
+    alert("Invalid choice. Please try again.");
+    length=parseInt(prompt("Choose a minimum of 8 characters and a maximum of 128 characters."));
   }
 
+  if (generateBtn => 8 && generateBtn <= 128) {
+ number =confirm("Would you like your password to include numbers?")
+ Upper =confirm("Would you like your password to include Uppercase letters?")
+ lower =confirm("Would you like your password to include lowercase letters?")
+ symbols =confirm("Would you like your password to include special characters?")
+  
+  
+  }
+
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
- 
+ passwordText.value = password;
 
-passwordText.value = password;
-
-
+ passwordEntry.textContent=randomPswdString
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword).value;
+var generateBtn=document.getElementById("generateBtn");
+var passwordEntry=document.getElementById("passwordEntry");
+generateBtn.onclick=generatePassword;
