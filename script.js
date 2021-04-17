34e99d57e3f391e1f1e7dd20feb810ae5c8e7c75
lissy <slissy513@gmail.com>
2021-04-16 22:36:59 -0400

    added more prompts to my javascript while simultaneously debugging prompts

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var length 
+var Strings = [];
 var numberString = "0123456789";
 var UpperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerString = "abcdefghijklmnopqrstuvwxyz";
 var symbolsString = "!@#$%^&*(){}[]=<>/,.";
-var randomPswdString = '';
+
 // Write password to the #password input
 function generatePassword() {
   var generateBtn = prompt("Choose a minimum of 8 characters and a maximum of 128 characters.");
 
   if (generateBtn < 8 || generateBtn > 128) {
-    
+    alert("Invalid choice. Please try again.") 
   }
+   else {
+
   
- 
+  var numberString = confirm("Would you like your password to include numbers?");
+  if(numberString === true) {
+    for(var i = 0; i < numberString.length; i++)
+    Strings.push(numberString[i]);
+    }
+  
+  var UpperString = confirm("Would you like your password to include Uppercase letters?");
+  if (UpperString === true) {
+    for (var i = 0; i < UpperString.length; i++);
+  }
+  var lowerString = confirm("Would you like your password to include lowercase letters?");
+  if (lowerString === true) {
+    for (var i = 0; i < lowerString.length; i++);
+  }
+  var symbolsString = confirm("Would you like your password to include special characters?");
+  if (symbolsString === true) {
+    for (var i = 0; i < symbolsString.length; i++);
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  var allowed = {};
-  if (number) password + (allowed.number = "1234567890");
+ 
 
 passwordText.value = password;
 
