4b029485b2debc4268b0487b7c19fcf7d07dceec
lissy <slissy513@gmail.com>
2021-04-17 00:47:36 -0400

    Switched my 'strings' to 'arrays' and removed the entire first 'function' section; got my prompt fixed so now the window pops up when clicked on

--- script.js
+++ script.js
@@ -1,53 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Different Arrays
-var numberString = "0123456789";
-var UpperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerString = "abcdefghijklmnopqrstuvwxyz";
-var symbolsString = "!@#$%^&*(){}[]=<>/,.";
-var randomPswdString ='';
-
- 
-var number;
-var Upper;
-var lower;
-var symbols;
-
-// Write password to the #password input
 function generatePassword() {
-  var generateBtn = prompt("Choose a minimum of 8 characters and a maximum of 128 characters.");
 
-  if(generateBtn < 8 || generateBtn > 128) {
-    alert("Invalid choice. Please try again.");
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var symbolArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var UpperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var OutcomeArray = [];
+
+  var Passwordlength = prompt("Choose a minimum of 8 characters and a maximum of 128 characters.");
+  var lower =confirm ("Would you like lower case letters in your password?");
+  var Upper =confirm ("Would you like upper case letters in your password?");
+  var symbol =confirm ("Would you like symbols in your password?");
+  var numbers =confirm ("Would you like numbers in your password?");
+  if (numbers){
+    result
   }
-
-  if (generateBtn => 8 && generateBtn <= 128) {
- number =confirm("Would you like your password to include numbers?")
- Upper =confirm("Would you like your password to include Uppercase letters?")
- lower =confirm("Would you like your password to include lowercase letters?")
- symbols =confirm("Would you like your password to include special characters?")
-}
-
-if (number === true) {
-  var numberString;
-}
-
-if (Upper === true) {
-  UpperString = ""
-}
-
-  randomPswdString = (numberString + UpperString + lowerString + symbolsString)
-
-  function writePassword() {
+} 
+// Write password to the #password inpu
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
  passwordText.value = password;
-
- passwordEntry.textContent=randomPswdString
-
+  }
 // Add event listener to generate button
-var generateBtn=document.getElementById("generateBtn");
-var passwordEntry=document.getElementById("passwordEntry");
-generateBtn.onclick=generatePassword;
+generateBtn.addEventListener("click", writePassword);
